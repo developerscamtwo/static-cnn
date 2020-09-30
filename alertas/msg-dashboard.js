70639510c66c7b2a719f889b3db49c1608db3f54
developerscamtwo <68964516+developerscamtwo@users.noreply.github.com>
2020-09-30 08:53:46 -0300

    Update msg-dashboard.js

--- alertas/msg-dashboard.js
+++ alertas/msg-dashboard.js
@@ -1,7 +1,6 @@
 if(localStorage){
-	localStorage.removeItem('cnn-live');
-	const lido = localStorage.getItem('cnn-sprint120');
-
+	localStorage.removeItem('cnn-sprint120');
+	const lido = localStorage.getItem('cnn-sprint124');
 	if(!lido){
 		$('body').append(`<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-md" role="document">
@@ -17,10 +16,11 @@ if(localStorage){
 							<h2>Conheça nossas novidades!</h2>	
 							<div class="space-20"></div>					
 							<ul>
-								<li>Integração com meio de pagamento GalaxPay e possibilidade de envio de link de pagamento ao paciente.(<a href="https://youtu.be/1pe1M_xjXtc" target="_blank">Veja o vídeo</a>)</li>
-								<li>Nova versão da guia TISS 3.05.00</li>
-								<li>Relatório de horas consumidas pelo teleconsulta</li>
-								<li>Desconto do custo do procedimento para cálculo de comissionamento</li>
+								<li>Alterado o nome da comissão de "Execução" para "Paciente"</li>
+								<li>Possibilidade de cobrança recorrente com Galax Pay no plano de benefícios</li>
+								<li>Imagens e documentos por beneficiário no plano de benefícios</li>
+								<li>Prioridades para os cards do painel de chamadas</li>
+								<li>Novo relatório customizável de agendamentos por status</li>
 							</ul>
 							<div class="space-10"></div>					
 							<a class="btn btn-link btn-block" target="_blank" href="https://clinicanasnuvens.movidesk.com/kb/pt-br/article/131714/4-120-28-de-julho-de-2020?menuId=9298-27492-131714">Saiba mais</a>
@@ -31,7 +31,6 @@ if(localStorage){
 		</div>
 	</div>`);
 	$('#md-message').modal('show');
-
-		localStorage.setItem('cnn-sprint120', "lido");
+		localStorage.setItem('cnn-sprint124', "lido");
 	}
 }
