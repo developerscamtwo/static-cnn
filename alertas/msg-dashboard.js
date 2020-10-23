24e7ff4a15825eb3e68b34a9784415cf9bdf30fe
developerscamtwo <68964516+developerscamtwo@users.noreply.github.com>
2020-10-23 17:25:25 -0300

    commit sprint 125

--- alertas/msg-dashboard.js
+++ alertas/msg-dashboard.js
@@ -1,6 +1,6 @@
 if(localStorage){
-	localStorage.removeItem('cnn-sprint120');
-	const lido = localStorage.getItem('cnn-sprint124');
+	localStorage.removeItem('cnn-sprint124');
+	const lido = localStorage.getItem('cnn-sprint125');
 	if(!lido){
 		$('body').append(`<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-md" role="document">
@@ -10,27 +10,27 @@ if(localStorage){
 						<div class="col-sm-5 no-padding-left hidden-xs">
 						<img class="img-responsive center" src="http://camtwo.com.br/magraum/novidade-121.png" alt="">
 						</div>
-						<div class="col-sm-7">				
-							<button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
+						<div class="col-sm-7">              
+                            <button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
 <div class="space-20"></div>
-							<h2>Conheça nossas novidades!</h2>	
-							<div class="space-20"></div>					
-							<ul>
-								<li>Alterado o nome da comissão de "Execução" para "Paciente"</li>
-								<li>Possibilidade de cobrança recorrente com Galax Pay no plano de benefícios</li>
-								<li>Imagens e documentos por beneficiário no plano de benefícios</li>
-								<li>Prioridades para os cards do painel de chamadas</li>
-								<li>Novo relatório customizável de agendamentos por status</li>
-							</ul>
-							<div class="space-10"></div>					
-							<a class="btn btn-link btn-block" target="_blank" href="https://clinicanasnuvens.movidesk.com/kb/pt-br/article/131714/4-120-28-de-julho-de-2020?menuId=9298-27492-131714">Saiba mais</a>
-						</div>
+                            <h2>Conheça nossas novidades!</h2> 
+                            <div class="space-20"></div>                  
+                            <ul>
+                                <li>Nova função para clínicas odontológicas: <b>Planos de tratamento</b></li>                   
+                                <li>Inserida data de criação e atualização para regras de comissões</li>
+                                <li>Melhorias na função "Esqueci minha senha" na tela de login</li>
+                                <li>Guias TISS geradas na agenda podem ser acessadas em "detalhes da agenda"</li>
+                                <li>Na edição da guia TISS, o sistema mostra o número do lote onde ela se encontra</li>
+                            </ul>
+                            <div class="space-10"></div>                  
+                            <a class="btn btn-link btn-block" target="_blank" href="https://clinicanasnuvens.movidesk.com/kb/article/147658/4-124-25-de-setembro-de-2020">Saiba mais</a>
+                        </div>
 					</div>
 				</div>	
 			</div>
 		</div>
 	</div>`);
 	$('#md-message').modal('show');
-		localStorage.setItem('cnn-sprint124', "lido");
+		localStorage.setItem('cnn-sprint125', "lido");
 	}
 }
