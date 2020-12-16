5245c0d04f3d7ece830c8b2b7bb7e2523d008bd4
developerscamtwo <68964516+developerscamtwo@users.noreply.github.com>
2020-12-16 19:55:40 -0300

    versao 128

--- alertas/msg-dashboard.js
+++ alertas/msg-dashboard.js
@@ -1,6 +1,6 @@
 if(localStorage){
-	localStorage.removeItem('cnn-sprint125');
-	const lido = localStorage.getItem('cnn-sprint127');
+	localStorage.removeItem('cnn-sprint127');
+	const lido = localStorage.getItem('cnn-sprint128');
 	if(!lido){
 		$('body').append(`<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-md" role="document">
@@ -14,25 +14,21 @@ if(localStorage){
                             <button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
 <div class="space-15"></div>
                             <h2>Conheça nossas novidades!</h2> 
-                            <div class="space-5"></div>                  
+                            <div class="space-20"></div>                  
                             <ul>
-                                <li>Agora é possível ao adicionar um compromisso ao profissional de saúde e repetir ele para demais dias.</li>                   
-                                <li>Criação de dietas para prescrição ao paciente</li>
-                                <li>Guias TISS de solicitação de internação</li>
-                                <li>Adicionado novos métodos de integração para a API</li>
-                                
+                                <li>Possibilidade de configurar e adicionar pastas padrão aos pacientes</li>                   
+                                <li>Impressão de guia odontológica</li>
                             </ul>
                             <p>Deseja conhecer estas e demais atualizações que preparamos, clique em Saiba mais!</p>
-                            <div class="space-5"></div>                  
-                            <a class="btn btn-link btn-block" target="_blank" href="https://clinicanasnuvens.movidesk.com/kb/article/160716/4-127-30-de-novembro-de-2020">Saiba mais</a>
+                            <div class="space-20"></div>                  
+                            <a class="btn btn-link btn-block" target="https://clinicanasnuvens.movidesk.com/kb/article/164431/4-128-17-de-dezembro">Saiba mais</a>
                         </div>
 					</div>
 					</div>
 				</div>	
 			</div>
-		</div>
 	</div>`);
 	$('#md-message').modal('show');
-		localStorage.setItem('cnn-sprint127', "lido");
+		localStorage.setItem('cnn-sprint128', "lido");
 	}
 }
