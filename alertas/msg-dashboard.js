cec4c7fc00392ce6590fe7cbcce4d8fe62dc3768
developerscamtwo <68964516+developerscamtwo@users.noreply.github.com>
2021-03-01 09:39:13 -0300

    versao 130

--- alertas/msg-dashboard.js
+++ alertas/msg-dashboard.js
@@ -1,6 +1,6 @@
 if(localStorage){
-	localStorage.removeItem('cnn-sprint128');
-	const lido = localStorage.getItem('cnn-sprint129');
+	localStorage.removeItem('cnn-sprint129');
+	const lido = localStorage.getItem('cnn-sprint130');
 	if(!lido){
 		$('body').append(`<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-md" role="document">
@@ -12,25 +12,28 @@ if(localStorage){
 						</div>
 						<div class="col-sm-7">              
                             <button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
-<div class="space-15"></div>
+<div class="space-5"></div>
                             <h2>Conheça nossas novidades!</h2> 
-                            <div class="space-20"></div>                  
+                            <div class="space-5"></div>                  
                             <ul>
-                                <li>Novo módulo para controle de guias e lotes TISS;</li>                   
-                                <li>Melhorias de usabilidade nas guias TISS;</li>
-                                <li>Novo recurso de glosa;</li>
-                                <li>Nova cobrança para convênios;</li>
+                                <li>Nomes dos procedimentos no TISS adequados caso for tabela própria;</li>                   
+                                <li>XML de recurso para TISS;</li>
+                                <li>Cliente/Fornecedor agora tem categoria padrão para pagamento e para recebimento;</li>
+                                <li>Melhorias na exibição de comissões no financeiro;</li>
+    <li>Nova situação de "perdido" para o orçamento;</li>
+    <li> Possibilidade de finalizar a venda de plano de benefícios;</li>
                             </ul>
                             <p>Para conhecer estas e demais atualizações que preparamos, clique em Saiba mais!</p>
-                            <div class="space-18"></div>                  
-                            <a class="btn btn-link btn-block" target="https://clinicanasnuvens.movidesk.com/kb/article/175184/4-129-9-de-fevereiro-2021">Saiba mais</a>
+                            <div class="space-2"></div>                  
+                            <a class="btn btn-link btn-block" target="https://clinicanasnuvens.movidesk.com/kb/article/180156/4-130-02-marco-2021">Saiba mais</a>
                         </div>
 					</div>
 					</div>
-				</div>	
+				</div>
 			</div>
+		</div>
 	</div>`);
 	$('#md-message').modal('show');
-		localStorage.setItem('cnn-sprint129', "lido");
+		localStorage.setItem('cnn-sprint130', "lido");
 	}
 }
