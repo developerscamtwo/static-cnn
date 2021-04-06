8ac65706c7fdc46b29b95ea3b3f9b85ef2ae1092
developerscamtwo <68964516+developerscamtwo@users.noreply.github.com>
2021-04-06 16:23:35 -0300

    atualizacao 131

--- alertas/msg-dashboard.js
+++ alertas/msg-dashboard.js
@@ -1,39 +1,37 @@
 if(localStorage){
-	localStorage.removeItem('cnn-sprint129');
-	const lido = localStorage.getItem('cnn-sprint130');
+	localStorage.removeItem('cnn-sprint130');
+	const lido = localStorage.getItem('cnn-sprint131');
 	if(!lido){
-		$('body').append(`<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
+		$('body').append(`
+	<div id="md-message" class="modal fade" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-md" role="document">
 			<div class="modal-content">
 				<div class="modal-body no-padding">
 					<div class="clearfix">
 						<div class="col-sm-5 no-padding-left hidden-xs">
-						<img class="img-responsive center" src="http://camtwo.com.br/magraum/novidade-121.png" alt="">
+							<img class="img-responsive center" src="http://camtwo.com.br/magraum/novidade-121.png" alt="">
 						</div>
 						<div class="col-sm-7">              
-                            <button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
-<div class="space-5"></div>
-                            <h2>Conheça nossas novidades!</h2> 
-                            <div class="space-5"></div>                  
-                            <ul>
-                                <li>Nomes dos procedimentos no TISS adequados caso for tabela própria;</li>                   
-                                <li>XML de recurso para TISS;</li>
-                                <li>Cliente/Fornecedor agora tem categoria padrão para pagamento e para recebimento;</li>
-                                <li>Melhorias na exibição de comissões no financeiro;</li>
-    <li>Nova situação de "perdido" para o orçamento;</li>
-    <li> Possibilidade de finalizar a venda de plano de benefícios;</li>
-                            </ul>
-                            <p>Para conhecer estas e demais atualizações que preparamos, clique em Saiba mais!</p>
-                            <div class="space-2"></div>                  
-                            <a class="btn btn-link btn-block" target="https://clinicanasnuvens.movidesk.com/kb/article/180156/4-130-02-marco-2021">Saiba mais</a>
-                        </div>
-					</div>
+                            				<button type="button" class="btn btn-danger pull-right btn-sm" data-dismiss="modal" aria-hidden="true">×</button>
+							<div class="space-5"></div>
+                            				<h2>Conheça nossas novidades!</h2> 
+                           				<div class="space-5"></div>                  
+                            				<ul>
+                               					<li>Antecipação de vendas em cartão de crédito</li>                   
+                                				<li>Possibilidade de conciliação manual no financeiro</li>
+                                				<li>Cadastro de feriados e exibição deles na agenda</li>
+                                				<li>Melhorias em como a senha é redefinida para adequar a Lei Geral de Proteção de Dados (LGPD)</li>
+                            				</ul>
+                            				<p>Para conhecer estas e demais atualizações que preparamos, clique em Saiba mais!</p>
+                            				<div class="space-2"></div>                  
+                            				<a class="btn btn-link btn-block" target="_blank" href="https://clinicanasnuvens.movidesk.com/kb/article/186144/4-131-07-abril-2021">Saiba mais</a>
+                        			</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>`);
 	$('#md-message').modal('show');
-		localStorage.setItem('cnn-sprint130', "lido");
+		localStorage.setItem('cnn-sprint131', "lido");
 	}
 }
